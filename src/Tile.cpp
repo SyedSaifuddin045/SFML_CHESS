9122af0c0db7c80a4f077362b82882eb3c68ff69
SyedSaifuddin045 <syedsaifuddin.vlcro@gmail.com>
2023-07-20 20:50:41 +0530

    Added System to also rest the tiles upon selecting another piece

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -7,8 +7,6 @@ Tile::Tile()
 Tile::Tile(sf::Vector2f size, std::shared_ptr<sf::Texture> texture, sf::Vector2f position, sf::Vector2i boardPosition)
 	:t_Body(size)
 {
-	t_Body.setTexture(texture.get());
-	t_Body.setPosition(position);
 	t_Position = boardPosition;
 	//m_body.setOrigin(Size / 2.0f);
 }
